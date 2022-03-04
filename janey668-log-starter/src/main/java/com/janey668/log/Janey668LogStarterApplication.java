8fa6a7786e0d48ea8921dc93e54944b85e281731
陈江龙 <chenjianglong@afanticar.com>
2022-03-04 19:12:04 +0800

    log-starter init

---
+++ janey668-log-starter/src/main/java/com/janey668/log/Janey668LogStarterApplication.java
@@ -0,0 +1,13 @@
+package com.janey668.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janey668LogStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Janey668LogStarterApplication.class, args);
+    }
+
+}
---
+++ janey668-log-starter/src/test/java/com/janey668/log/Janey668LogStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janey668.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janey668LogStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
