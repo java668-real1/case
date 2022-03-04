0f7076644e4325e348cf4ecabff62c31b2879881
陈江龙 <chenjianglong@afanticar.com>
2022-03-04 19:20:37 +0800

    rename application

--- janey668-log-starter/src/main/java/com/janey668/log/LogStarterApplication.java
+++ janey668-log-starter/src/main/java/com/janey668/log/LogStarterApplication.java
@@ -3,11 +3,15 @@ package com.janey668.log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author Jerry.chen
+ */
 @SpringBootApplication
-public class Janey668LogStarterApplication {
+public class LogStarterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Janey668LogStarterApplication.class, args);
+        SpringApplication.run(LogStarterApplication.class, args);
     }
 
 }
