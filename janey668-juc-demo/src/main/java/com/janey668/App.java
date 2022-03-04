d6b25752a433f62a3e272eb31a3864273e5bbfec
陈江龙 <zealer_team@163.com>
2022-03-04 23:06:24 +0800

    init juc demo

---
+++ janey668-juc-demo/src/main/java/com/janey668/App.java
@@ -0,0 +1,13 @@
+package com.janey668;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
