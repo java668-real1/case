74107441f77b61319a24dfb6551a4d46df779b1f
陈江龙 <chenjianglong@afanticar.com>
2022-03-16 18:18:09 +0800

    add netty app java

---
+++ janey668-juc-demo/src/main/java/com/janey668/c1/NotifyTest3.java
@@ -0,0 +1,48 @@
+package com.janey668.c1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static java.lang.Thread.sleep;
+
+@Slf4j
+public class NotifyTest3 {
+    static final Object room = new Object();
+    static boolean hasCigarette = false;
+    static boolean hasTakeout = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            synchronized (room) {
+                log.debug("有烟没？[{}]", hasCigarette);
+                if (!hasCigarette) {
+                    log.debug("没烟，先歇会！");
+                    try {
+                        room.wait(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                log.debug("有烟没？[{}]", hasCigarette);
+                if (hasCigarette) {
+                    log.debug("可以开始干活了");
+                }
+            }
+        }, "小南").start();
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                synchronized (room) {
+                    log.debug("可以开始干活了");
+                }
+            }, "其它人").start();
+        }
+        sleep(1);
+        new Thread(() -> {
+            synchronized (room) {
+                hasCigarette = true;
+                log.debug("烟到了噢！");
+                room.notify();
+            }
+        }, "送烟的").start();
+    }
+
+}
