ef249b06cde525f5813a443555f3443a1b75ae6e
陈江龙 <zealer_team@163.com>
2022-07-06 23:32:30 +0800

    channel test

--- janey668-netty-demo/src/main/java/com/janey668/netty/channel/ChannelNettyServer.java
+++ janey668-netty-demo/src/main/java/com/janey668/netty/channel/ChannelNettyServer.java
@@ -1,6 +1,8 @@
 package com.janey668.netty.channel;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.*;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -8,6 +10,8 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 public class ChannelNettyServer {
 
@@ -19,22 +23,26 @@ public class ChannelNettyServer {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new LoggingHandler(LogLevel.DEBUG));
                         pipeline.addLast(new StringDecoder());
                         pipeline.addLast(new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                 System.out.println(msg);
-                                ctx.channel().writeAndFlush("服务器接收了：" + msg);
+                                String message = "服务器接收了：" + msg;
+                                ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+                                buffer.writeBytes(message.getBytes());
+                                ctx.channel().writeAndFlush(buffer);
                             }
                         });
 //                        pipeline.addLast(new StringEncoder());
-                        pipeline.addLast(new ChannelOutboundHandlerAdapter() {
-                            @Override
-                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-                                System.out.println(msg);
-                                super.write(ctx, msg, promise);
-                            }
-                        });
+//                        pipeline.addLast(new ChannelOutboundHandlerAdapter() {
+//                            @Override
+//                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+//                                System.out.println(msg);
+//                                super.write(ctx, msg, promise);
+//                            }
+//                        });
                     }
                 })
                 .bind(8080);
