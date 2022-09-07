9fc7959b506d316d0975bae7297532f13db28835
陈江龙 <chenjianglong@afanticar.com>
2022-09-07 19:25:15 +0800

    mybatis plus demo commit

--- janey668-mybatisplus-demo/src/main/java/com/janey668/mybatisplus/po/User.java
+++ janey668-mybatisplus-demo/src/main/java/com/janey668/mybatisplus/po/User.java
@@ -1,11 +1,19 @@
 package com.janey668.mybatisplus.po;
 
+import com.baomidou.mybatisplus.annotation.Version;
 import lombok.Data;
 
 @Data
 public class User {
+
     private Long id;
     private String name;
     private Integer age;
     private String email;
+
+    /**
+     * 乐观锁version注解
+     */
+    @Version
+    private Integer version;
 }
--- janey668-mybatisplus-demo/src/test/java/com/janey668/mybatisplus/SampleTest.java
+++ janey668-mybatisplus-demo/src/test/java/com/janey668/mybatisplus/SampleTest.java
@@ -24,4 +24,36 @@ public class SampleTest {
         userList.forEach(System.out::println);
     }
 
+    @Test
+    public void testMybatisPlusInterceptor(){
+        //1、查询用户信息
+        User user = userMapper.selectById(1L);
+        //2、修改用户信息
+        user.setName("zhangsan");
+        user.setEmail("963330213@qq.com");
+        //3、执行更新操作
+        userMapper.updateById(user);
+    }
+
+    //测试乐观锁失败
+    @Test
+    public void testMybatisPlusInterceptor2(){
+        //1、查询用户信息
+        User user = userMapper.selectById(1L);
+        //2、修改用户信息
+        user.setName("zhangsan1");
+        user.setEmail("963330213@qq.com");
+
+        //模拟另外一个线程执行了插队操作
+        User user2 = userMapper.selectById(1L);
+        user2.setName("zhangsan2");
+        user2.setEmail("963330213@qq.com");
+        int count1 = userMapper.updateById(user2);
+        Assert.assertEquals(1, count1);
+
+        //自旋锁来尝试多次提交
+        int count2 = userMapper.updateById(user2);//如果没有乐观锁就会覆盖插队线程的值！
+        Assert.assertEquals(0, count2);
+    }
+
 }
